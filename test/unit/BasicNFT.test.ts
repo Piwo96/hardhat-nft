8b0fe682401e760ad8eae8f7e12fd19190704b62
Piwo96 <philipp.rollmann@online.de>
2022-08-22 15:20:03 +0200

    Finished unit tests for BasicNFT.sol

--- test/unit/BasicNFT.test.ts
+++ test/unit/BasicNFT.test.ts
@@ -1,5 +1,6 @@
 import { assert, expect } from "chai";
 import { network, ethers, deployments } from "hardhat";
+import { BigNumber } from "ethers";
 import { BasicNFT } from "../../typechain-types";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { developmentChains } from "../../helper-hardhat-config";
@@ -37,6 +38,29 @@ import { developmentChains } from "../../helper-hardhat-config";
               it("Has a token counter", async function () {
                   const counter = await basicNft.getTokenCounter();
                   assert.equal(counter.toString(), "0");
+              });
+          });
+
+          describe("mintNft", function () {
+              it("Increments token counter", async function () {
+                  const counterBeforeMint = await basicNft.getTokenCounter();
+                  await basicNft.mintNft();
+                  const counterAfterMint = await basicNft.getTokenCounter();
+                  assert.equal(
+                      counterBeforeMint.add(BigNumber.from("1")).toString(),
+                      counterAfterMint.toString()
+                  );
+              });
+
+              it("Deployer has a balance of 0 when not minted", async function () {
+                  const balance = await basicNft.balanceOf(deployer.address);
+                  assert.equal(balance.toString(), "0");
+              });
+
+              it("Deployer has a balance of 1 after mint", async function () {
+                  await basicNft.mintNft();
+                  const balance = await basicNft.balanceOf(deployer.address);
+                  assert.equal(balance.toString(), "1");
               });
           });
       });
