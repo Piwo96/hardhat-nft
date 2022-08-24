e4189c6ca8f67f8d34ab06268b25f4e3de953d97
Piwo96 <philipp.rollmann@online.de>
2022-08-24 22:55:35 +0200

    added deploy script for mocks

---
+++ deploy/00-deploy-mocks.ts
@@ -0,0 +1,24 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import { developmentChains } from "../helper-hardhat-config";
+import { ethers } from "hardhat";
+
+const BASE_FEE = ethers.utils.parseEther("0.25");
+const GAS_PRICE_LINK = 1e9;
+
+const deployMocks: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+    const { getNamedAccounts, deployments, network } = hre;
+    const { deploy, log } = deployments;
+    const { deployer } = await getNamedAccounts();
+
+    if (developmentChains.includes(network.name)) {
+        await deploy("VRFCoordinatorV2Mock", {
+            contract: "VRFCoordinatorV2Mock",
+            from: deployer,
+            log: true,
+            args: [BASE_FEE, GAS_PRICE_LINK],
+        });
+        log("Mock deployed!");
+        log("---------------------------------");
+    }
+};
