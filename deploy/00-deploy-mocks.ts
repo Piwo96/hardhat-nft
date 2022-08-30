f7835e868f26b5f503b50020c5171e6f8629b231
Piwo96 <philipp.rollmann@online.de>
2022-08-30 16:41:51 +0200

    added deployment logic for MockV3Aggregator

--- deploy/00-deploy-mocks.ts
+++ deploy/00-deploy-mocks.ts
@@ -3,6 +3,9 @@ import { DeployFunction } from "hardhat-deploy/types";
 import { developmentChains } from "../helper-hardhat-config";
 import { ethers } from "hardhat";
 
+const DECIMALS = "18";
+const INITIAL_PRICE = ethers.utils.parseUnits("2000", "ether");
+
 const BASE_FEE = ethers.utils.parseEther("0.25");
 const GAS_PRICE_LINK = 1e9;
 
@@ -17,6 +20,13 @@ const deployMocks: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
             from: deployer,
             log: true,
             args: [BASE_FEE, GAS_PRICE_LINK],
+        });
+
+        await deploy("MockV3Aggregator", {
+            contract: "MockV3Aggregator",
+            from: deployer,
+            log: true,
+            args: [DECIMALS, INITIAL_PRICE],
         });
         log("Mock deployed!");
         log("---------------------------------");
