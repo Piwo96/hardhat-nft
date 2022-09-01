e1c45f8aeed931e0c5343861a22b5cc0da57bf16
Piwo96 <philipp.rollmann@online.de>
2022-09-01 14:32:26 +0200

    added mintscript

---
+++ deploy/04-mint.ts
@@ -0,0 +1,69 @@
+import { ethers, network, getNamedAccounts } from "hardhat";
+import { resolve } from "path";
+import {
+    BasicNFT,
+    RandomIpfsNFT,
+    DynamicSvgNft,
+    VRFCoordinatorV2Mock,
+} from "../typechain-types";
+import { developmentChains } from "../helper-hardhat-config";
+import { DeployFunction } from "hardhat-deploy/types";
+
+const mint: DeployFunction = async () => {
+    const { deployer } = await getNamedAccounts();
+
+    // Basic NFT
+    const basicNft: BasicNFT = await ethers.getContract("BasicNFT", deployer);
+    const basicMintTx = await basicNft.mintNft();
+    await basicMintTx.wait(1);
+    console.log(
+        `Basic NFT index 0 has tokenURI: ${await basicNft.tokenURI(0)}`
+    );
+
+    // RandomIpfsNFT
+
+    const randomIpfsNft: RandomIpfsNFT = await ethers.getContract(
+        "RandomIpfsNFT",
+        deployer
+    );
+    const mintFee = await randomIpfsNft.getMintFee();
+    await new Promise<void>(async (resolve, reject) => {
+        setTimeout(resolve, 300000);
+        randomIpfsNft.once("NftRequested", async function () {
+            try {
+                resolve();
+            } catch (error) {
+                reject();
+            }
+        });
+        const randomMintTx = await randomIpfsNft.requestNft({ value: mintFee });
+        const randomMintTxReceipt = await randomMintTx.wait(1);
+        if (developmentChains.includes(network.name)) {
+            const requestId = randomMintTxReceipt.events![1].args!.requestId;
+            const vrfCoordinatorV2Mock: VRFCoordinatorV2Mock =
+                await ethers.getContract("VRFCoordinatorV2Mock", deployer);
+            await vrfCoordinatorV2Mock.fulfillRandomWords(
+                requestId,
+                randomIpfsNft.address
+            );
+        }
+    });
+    console.log(
+        `Random IPFS NFT index 0 tokenURI: ${await randomIpfsNft.tokenURI(0)}`
+    );
+
+    // Dynamic SVG NFT
+
+    const highValue = ethers.utils.parseEther("4000");
+    const dynamicSvgNft: DynamicSvgNft = await ethers.getContract(
+        "DynamicSvgNft",
+        deployer
+    );
+    const dynamicMintTx = await dynamicSvgNft.mintNft(highValue);
+    await dynamicMintTx.wait(1);
+    console.log(
+        `Dynamic SVG NFT index 0 tokenURI: ${await dynamicSvgNft.tokenURI(1)}`
+    );
+};
+
+export default mint;
