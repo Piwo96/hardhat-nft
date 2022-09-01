9df71c0f08ed07691ef365ccacfd82300d8d8578
Piwo96 <philipp.rollmann@online.de>
2022-09-01 15:41:28 +0200

    fixed issues

--- deploy/04-mint.ts
+++ deploy/04-mint.ts
@@ -13,44 +13,44 @@ const mint: DeployFunction = async () => {
     const { deployer } = await getNamedAccounts();
 
     // Basic NFT
-    const basicNft: BasicNFT = await ethers.getContract("BasicNFT", deployer);
-    const basicMintTx = await basicNft.mintNft();
-    await basicMintTx.wait(1);
-    console.log(
-        `Basic NFT index 0 has tokenURI: ${await basicNft.tokenURI(0)}`
-    );
+    // const basicNft: BasicNFT = await ethers.getContract("BasicNFT", deployer);
+    // const basicMintTx = await basicNft.mintNft();
+    // await basicMintTx.wait(1);
+    // console.log(
+    //     `Basic NFT index 0 has tokenURI: ${await basicNft.tokenURI(0)}`
+    // );
 
     // RandomIpfsNFT
 
-    const randomIpfsNft: RandomIpfsNFT = await ethers.getContract(
-        "RandomIpfsNFT",
-        deployer
-    );
-    const mintFee = await randomIpfsNft.getMintFee();
-    await new Promise<void>(async (resolve, reject) => {
-        setTimeout(resolve, 300000);
-        randomIpfsNft.once("NftRequested", async function () {
-            try {
-                resolve();
-            } catch (error) {
-                reject();
-            }
-        });
-        const randomMintTx = await randomIpfsNft.requestNft({ value: mintFee });
-        const randomMintTxReceipt = await randomMintTx.wait(1);
-        if (developmentChains.includes(network.name)) {
-            const requestId = randomMintTxReceipt.events![1].args!.requestId;
-            const vrfCoordinatorV2Mock: VRFCoordinatorV2Mock =
-                await ethers.getContract("VRFCoordinatorV2Mock", deployer);
-            await vrfCoordinatorV2Mock.fulfillRandomWords(
-                requestId,
-                randomIpfsNft.address
-            );
-        }
-    });
-    console.log(
-        `Random IPFS NFT index 0 tokenURI: ${await randomIpfsNft.tokenURI(0)}`
-    );
+    // const randomIpfsNft: RandomIpfsNFT = await ethers.getContract(
+    //     "RandomIpfsNFT",
+    //     deployer
+    // );
+    // const mintFee = await randomIpfsNft.getMintFee();
+    // await new Promise<void>(async (resolve, reject) => {
+    //     setTimeout(resolve, 300000);
+    //     randomIpfsNft.once("NftMinted", async function () {
+    //         try {
+    //             resolve();
+    //         } catch (error) {
+    //             reject(error);
+    //         }
+    //     });
+    //     const randomMintTx = await randomIpfsNft.requestNft({ value: mintFee });
+    //     const randomMintTxReceipt = await randomMintTx.wait(1);
+    //     if (developmentChains.includes(network.name)) {
+    //         const requestId = randomMintTxReceipt.events![1].args!.requestId;
+    //         const vrfCoordinatorV2Mock: VRFCoordinatorV2Mock =
+    //             await ethers.getContract("VRFCoordinatorV2Mock", deployer);
+    //         await vrfCoordinatorV2Mock.fulfillRandomWords(
+    //             requestId,
+    //             randomIpfsNft.address
+    //         );
+    //     }
+    // });
+    // console.log(
+    //     `Random IPFS NFT index 0 tokenURI: ${await randomIpfsNft.tokenURI(0)}`
+    // );
 
     // Dynamic SVG NFT
 
